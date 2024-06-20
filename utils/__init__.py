7b45d57385a2aba39a494141b3c5fccdf44d706b
ggllllll <143251229+ggllllll@users.noreply.github.com>
2024-06-20 22:09:04 +0800

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-from .iter_counter import IterationCounter
-from .visualizer import Visualizer
-from .metric_tracker import MetricTracker
-from .metrics import mean_dice
-# from .distribution_estimation import kmeans, pairwise_cosine
-from .masking import FixedThresholding,SoftMatchWeighting
-from .dist_align import DistAlignEMA
