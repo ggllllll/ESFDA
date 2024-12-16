2dd8915ce25e6654169301cceea6f198e5ef9f17
ggllllll <143251229+ggllllll@users.noreply.github.com>
2024-12-16 11:50:20 +0800

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -30,19 +30,19 @@ class DiceLoss(nn.Module):
         super(DiceLoss, self).__init__()
 
     def forward(self, pred, target, weight=None):
-        smooth = 1        #防止分母为0
+        smooth = 1     
 
-        size = pred.size(0)         #获取batchsize的大小
+        size = pred.size(0)         
 
         pred_flat = pred.view(size, -1)
-        target_flat = target.view(size, -1)          #将预测张量和目标张量展平成为二维张量
+        target_flat = target.view(size, -1)         
 
-        intersection = pred_flat * target_flat       #计算它们之间的交集
-        dice_score = (2 * intersection.sum(1) + smooth)/(pred_flat.sum(1) + target_flat.sum(1) + smooth)       #计算dice值
+        intersection = pred_flat * target_flat      
+        dice_score = (2 * intersection.sum(1) + smooth)/(pred_flat.sum(1) + target_flat.sum(1) + smooth)  
         if weight is not None:
             #print(weight)
-            dice_score = weight * dice_score         #将权重与dice相乘重新得到dice的值
-            dice_loss = weight.sum() /size - dice_score.sum()/size       #计算dice_loss
+            dice_score = weight * dice_score      
+            dice_loss = weight.sum() /size - dice_score.sum()/size    
         else:
             dice_loss = 1 - dice_score.sum()/size
 
@@ -50,28 +50,27 @@ class DiceLoss(nn.Module):
 
 
 def smooth_loss(y_true, y_pred, weight=None):
-    H = y_pred.shape[-2]     #得到分割图像上的高
-    W = y_pred.shape[-1]     #得到分割图像上的宽
+    H = y_pred.shape[-2]    
+    W = y_pred.shape[-1]    
 
-    #计算损失
-           #计算水平(后两行）垂直（前两行）方向上相邻像素点差值
+
     loss = torch.abs(y_pred[:, 1:H - 1, 1:W - 1] - y_pred[:, 0:H - 2, 1:W - 1]) + \
            torch.abs(y_pred[:, 1:H - 1, 1:W - 1] - y_pred[:, 2:H, 1:W - 1]) + \
            torch.abs(y_pred[:, 1:H - 1, 1:W - 1] - y_pred[:, 1:H - 1, 0:W - 2]) + \
            torch.abs(y_pred[:, 1:H - 1, 1:W - 1] - y_pred[:, 1:H - 1, 2:W])
-          #计算真是标签中平滑函数的权重系数M1、M2、M3、M4
-    M1 = (y_true[:, 1:H - 1, 1:W - 1] == y_true[:, 0:H - 2, 1:W - 1]).to(dtype=torch.float32)     #计算垂直方向上的相邻像素之间的权重
-    M2 = (y_true[:, 1:H - 1, 1:W - 1] == y_true[:, 2:H, 1:W - 1]).to(dtype=torch.float32)         #计算垂直方向上的相邻像素之间的权重
-    M3 = (y_true[:, 1:H - 1, 1:W - 1] == y_true[:, 1:H - 1, 0:W - 2]).to(dtype=torch.float32)     #计算水平方向上相邻像素之间的权重
-    M4 = (y_true[:, 1:H - 1, 1:W - 1] == y_true[:, 1:H - 1, 2:W]).to(dtype=torch.float32)         #计算水平方向上相邻像素之间的权重
 
-    mask = M1 * M2 * M3 * M4          #将四个权重系数相乘得到一个新的掩码  #
-    loss = loss * mask                #预测计算的损失与掩码相乘
-    loss = loss.mean(dim=[1,2])       #计算的损失在第一第二个维度上取平均值
+    M1 = (y_true[:, 1:H - 1, 1:W - 1] == y_true[:, 0:H - 2, 1:W - 1]).to(dtype=torch.float32)    
+    M2 = (y_true[:, 1:H - 1, 1:W - 1] == y_true[:, 2:H, 1:W - 1]).to(dtype=torch.float32)        
+    M3 = (y_true[:, 1:H - 1, 1:W - 1] == y_true[:, 1:H - 1, 0:W - 2]).to(dtype=torch.float32)     
+    M4 = (y_true[:, 1:H - 1, 1:W - 1] == y_true[:, 1:H - 1, 2:W]).to(dtype=torch.float32)       
+
+    mask = M1 * M2 * M3 * M4         
+    loss = loss * mask               
+    loss = loss.mean(dim=[1,2])     
     if weight is not None:
-        loss = loss * weight          #将损失在与权重相乘
+        loss = loss * weight       
 
-    loss = loss.mean()                #计算损失的平均值
+    loss = loss.mean()              
 
     return loss
 
